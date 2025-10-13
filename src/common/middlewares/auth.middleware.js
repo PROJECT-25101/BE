88226cab77a3555b6de44109c4b4a8289a7a90e0
QuocDL <quoclcph18659@gmail.com>
2025-10-13 10:32:59 +0700

    hotfix auth middleware

--- src/common/middlewares/auth.middleware.js
+++ src/common/middlewares/auth.middleware.js
@@ -1,25 +1,28 @@
 import jwt from "jsonwebtoken";
-import { throwError } from "../utils/create-response";
+import { throwError } from "../utils/create-response.js";
 
 export const authenticate = (secret) => {
   return (req, res, next) => {
     try {
+      if (!secret) {
+        return throwError(500, "NOT HAVE SECRECT KEY BACKEND DEV");
+      }
       const authHeader = req.headers.authorization;
       if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        throwError(400, "Token không tồn tại hoặc sai định dạng");
+        return throwError(400, "Token không tồn tại hoặc sai định dạng");
       }
       const token = authHeader.split(" ")[1];
       const decoded = jwt.verify(token, secret);
       req.user = decoded;
-      next();
+      return next();
     } catch (error) {
       if (error.name === "TokenExpiredError") {
-        throwError(401, "Token đã hết hạn");
+        return throwError(401, "Token đã hết hạn");
       }
       if (error.name === "JsonWebTokenError") {
-        throwError(401, "Token xác thực không hợp lệ");
+        return throwError(401, "Token xác thực không hợp lệ");
       }
-      throwError(500, "Lỗi không xác định");
+      return throwError(500, "Lỗi không xác định");
     }
   };
 };
