e5de0b255d5ae83b01ffcd6b87d549c056f57d1e
QuocDL <quoclcph18659@gmail.com>
2025-11-10 15:36:13 +0700

    FIX ROUTE

--- src/modules/route/route.utils.js
+++ src/modules/route/route.utils.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { regexLower } from "../../common/utils/regex.js";
 import Route from "./route.model.js";
 
@@ -7,11 +8,53 @@ export const checkDuplicateRoute = async (
   description,
   excludeId = null,
 ) => {
-  const condition = {
+  const baseCondition = {
     "pickupPoint.label": regexLower(pickupPoint.label),
     "dropPoint.label": regexLower(dropPoint.label),
     description: regexLower(description),
   };
-  if (excludeId) condition._id = { $ne: excludeId };
-  return Route.findOne(condition);
+  if (excludeId) {
+    baseCondition._id = { $ne: new mongoose.Types.ObjectId(excludeId) };
+  }
+  const routes = await Route.find(baseCondition).lean();
+  console.log(routes);
+  if (!routes.length) return null;
+  for (const route of routes) {
+    const pickDistricts =
+      pickupPoint.district?.map((d) => d.label.toLowerCase()) || [];
+    const dropDistricts =
+      dropPoint.district?.map((d) => d.label.toLowerCase()) || [];
+    const pickDistrictExist = route.pickupPoint?.district?.some((d) =>
+      pickDistricts.includes(d.label.toLowerCase()),
+    );
+    const dropDistrictExist = route.dropPoint?.district?.some((d) =>
+      dropDistricts.includes(d.label.toLowerCase()),
+    );
+    if (pickDistrictExist && dropDistrictExist) {
+      return route;
+    }
+  }
+  return null;
 };
+
+export function checkDistrictDuplicate(districts) {
+  const labelSet = new Set();
+  const descSet = new Set();
+
+  for (const d of districts) {
+    const labelLower = regexLower(d.label);
+    if (labelSet.has(labelLower)) {
+      return true;
+    }
+    labelSet.add(labelLower);
+    (d.description || []).forEach((desc) => {
+      const descLower = regexLower(desc);
+      if (descSet.has(descLower)) {
+        return true;
+      }
+      descSet.add(descLower);
+    });
+  }
+
+  return false;
+}
