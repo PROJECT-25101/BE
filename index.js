134c238de4fb4c908550957e64c4e764625bf715
QuocDL <quoclcph18659@gmail.com>
2025-11-20 11:40:06 +0700

    UNLIMIT JSON PAYLOAD

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ checkVersion();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cors());
 
 if (NODE_ENV === "development") {
